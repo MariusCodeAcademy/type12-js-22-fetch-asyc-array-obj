156ac1d227d9913ea7a09b4ecf1c506a9debbd9d
Marius <marius@example.com>
2022-08-04 08:49:23 +0300

    async no catch

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ const url = 'https://reqres.in/api/users/5';
 
 // gauti ir iskonsolinti sito user objekta
 
-fetch(url)
-  .then((resp) => resp.json())
-  .then((dataInJs) => console.log('dataInJs ===', dataInJs.data))
-  .catch((err) => console.warn(err));
+// fetch(url)
+//   .then((resp) => resp.json())
+//   .then((dataInJs) => console.log('dataInJs ===', dataInJs.data))
+//   .catch((err) => console.warn(err));
+
+// async ir await
+async function getUser() {
+  const resp = await fetch(url);
+  const dataInJs = await resp.json();
+  console.log('dataInJs async ir await ===', dataInJs.data);
+}
+getUser();
