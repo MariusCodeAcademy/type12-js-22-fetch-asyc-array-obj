6f10b240cd0cdbbffd54fdce12d4d16bbb6d2b18
Marius <marius@example.com>
2022-08-04 08:44:12 +0300

    promise fetch user

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 'use strict';
 console.log('main.js');
+
+// fetch
+const url = 'https://reqres.in/api/users/5';
+
+// gauti ir iskonsolinti sito user objekta
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((dataInJs) => console.log('dataInJs ===', dataInJs.data))
+  .catch((err) => console.warn(err));
